c9739f7f70b97e18db43912df9f6f2761a6458b7
stophin <kittybella@163.com>
2017-11-21 18:47:04 +0800

    Convolutional Neural Networks Data Structure : BASE

---
+++ CNN/CNN.cpp
@@ -0,0 +1,12 @@
+// CNN.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "common/MultiLinkList.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Hello world!");
+	getch();
+	return 0;
+}
+
---
+++ CNN/platform/ELibrary.h
@@ -0,0 +1,20 @@
+//ELibrary.h
+//now we're using ege library tool
+//you may change this
+//once changed, associated files should be changed
+//Author: Stophin
+//2014.01.08
+//Ver: 0.01
+//
+#ifndef _ELIBRARY_H_
+#define _ELIBRARY_H_
+
+#include <Windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <conio.h>
+
+#define getch _getch
+
+#endif	//end of _ELIBRARY_H_
+//end of file
